8ebfaee1a0804b438a1459236fab5829151c274d
davidthus <dvdnicorici05@gmail.com>
2023-03-21 22:13:27 +0000

    add update task function for store

--- src/stores/boardStore.js
+++ src/stores/boardStore.js
@@ -17,21 +17,6 @@ export function addBoard(boardName) {
 	boards.update((prev) => [...prev, { boardName, columns: [] }]);
 }
 
-export function deleteBoard(index) {
-	boards.update((prev) => prev.filter((_, currentBoardIndex) => currentBoardIndex !== index));
-}
-
-export function updateBoard(updatedBoard, index) {
-	boards.update((prev) =>
-		prev.map((board, currentBoardIndex) => {
-			if (currentBoardIndex === index) {
-				return updatedBoard;
-			}
-			return board;
-		})
-	);
-}
-
 export function addColumn(columnName, boardIndex) {
 	boards.update((prev) =>
 		prev.map((board, currentBoardIndex) => {
@@ -50,6 +35,65 @@ export function addTask(newTask, boardIndex, columnIndex) {
 				return board.columns.map((column, currentColumnIndex) => {
 					if (currentColumnIndex === columnIndex) {
 						column.tasks = [...column.tasks, newTask];
+					}
+					return column;
+				});
+			}
+			return board;
+		})
+	);
+}
+
+export function updateBoard(updatedBoard, index) {
+	boards.update((prev) =>
+		prev.map((board, currentBoardIndex) => {
+			if (currentBoardIndex === index) {
+				return updatedBoard;
+			}
+			return board;
+		})
+	);
+}
+
+// columns dont get updated, boards can either delete or add them, or tasks can be added to them or deleted
+
+export function updateTask(updatedTask, boardIndex, columnIndex, oldColumnIndex, taskIndex) {
+	boards.update((prev) =>
+		prev.map((board, currentBoardIndex) => {
+			let taskStatusChanged = false;
+			if (currentBoardIndex === boardIndex) {
+				return board.columns.map((column, currentColumnIndex) => {
+					if (currentColumnIndex === oldColumnIndex && currentColumnIndex === columnIndex) {
+						column.tasks[taskIndex] = updatedTask;
+					} else if (currentColumnIndex === columnIndex) {
+						column.tasks = [...column.tasks, updatedTask];
+						taskStatusChanged = true;
+					}
+
+					return column;
+				});
+			}
+
+			if (taskStatusChanged) {
+				board.columns[oldColumnIndex].splice(taskIndex, 1);
+			}
+
+			return board;
+		})
+	);
+}
+
+export function deleteBoard(index) {
+	boards.update((prev) => prev.filter((_, currentBoardIndex) => currentBoardIndex !== index));
+}
+
+export function deleteTask(boardIndex, columnIndex, taskIndex) {
+	boards.update((prev) =>
+		prev.map((board, currentBoardIndex) => {
+			if (currentBoardIndex === boardIndex) {
+				return board.columns.map((column, currentColumnIndex) => {
+					if (currentColumnIndex === columnIndex) {
+						column.tasks.filter((task, currentTaskIndex) => currentTaskIndex !== taskIndex);
 					}
 					return column;
 				});
