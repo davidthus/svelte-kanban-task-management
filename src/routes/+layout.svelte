3a68e9f8d6a160d7845aa89ae456ebd253d1d377
davidthus <dvdnicorici05@gmail.com>
2023-03-26 10:38:16 +0100

    change: add dataUnsubscribe to root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
 	import Navbar from '$lib/navbar.svelte';
 	import Sidebar from '$lib/sidebar.svelte';
+	import { onDestroy } from 'svelte';
 	import '../app.css';
 	import { themeTypes } from '../constants/themeTypes';
 	import { data } from '../stores/dataStore';
 
-	data.subscribe((value) => {
+	const dataUnsubscribe = data.subscribe((value) => {
 		console.log(value);
 		if (value.theme === themeTypes.LIGHTTHEME) {
 			document.documentElement.classList.remove('dark');
@@ -13,6 +14,7 @@
 			document.documentElement.classList.add('dark');
 		}
 	});
+	onDestroy(dataUnsubscribe);
 </script>
 
 <Navbar />
