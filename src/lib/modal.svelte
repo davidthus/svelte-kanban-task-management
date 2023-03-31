f90313b69ff252024e43ee4528d19b3750a35bdf
davidthus <dvdnicorici05@gmail.com>
2023-04-01 00:32:12 +0100

    add: modal container (not working)

--- src/lib/modal.svelte
+++ src/lib/modal.svelte
@@ -17,21 +17,24 @@
 
 <button
 	on:click={closeModal}
-	class="fixed inset-0 w-screen h-screen flex items-center justify-center"
+	class="fixed z-50 inset-0 w-screen h-screen flex items-center justify-center bg-black opacity-50 mix-blend-normal"
 >
-	{#if modalType === modalTypes.VIEWTASK}
-		<ViewTask {modalDetails} />
-	{:else if modalType === modalTypes.ADDTASK}
-		<AddTask {modalDetails} />
-	{:else if modalType === modalTypes.EDITTASK}
-		<EditTask {modalDetails} />
-	{:else if modalType === modalTypes.EDITBOARD}
-		<EditBoard {modalDetails} />
-	{:else if modalType === modalTypes.ADDBOARD}
-		<AddBoard {modalDetails} />
-	{:else if modalType === modalTypes.DELETEBOARD}
-		<DeleteBoard {modalDetails} />
-	{:else if modalType === modalTypes.DELETETASK}
-		<DeleteTask {modalDetails} />
-	{/if}
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<article on:click|stopPropagation class="max-w-[480px] w-full rounded-md p-8 flex flex-col gap-6">
+		{#if modalType === modalTypes.VIEWTASK}
+			<ViewTask {modalDetails} />
+		{:else if modalType === modalTypes.ADDTASK}
+			<AddTask {modalDetails} />
+		{:else if modalType === modalTypes.EDITTASK}
+			<EditTask {modalDetails} />
+		{:else if modalType === modalTypes.EDITBOARD}
+			<EditBoard {modalDetails} />
+		{:else if modalType === modalTypes.ADDBOARD}
+			<AddBoard {modalDetails} />
+		{:else if modalType === modalTypes.DELETEBOARD}
+			<DeleteBoard {modalDetails} />
+		{:else if modalType === modalTypes.DELETETASK}
+			<DeleteTask {modalDetails} />
+		{/if}
+	</article>
 </button>
--- src/lib/modals/viewTask.svelte
+++ src/lib/modals/viewTask.svelte
@@ -1,6 +1,8 @@
 <script>
-	import Modal from '../modal.svelte';
-
+	import { boards } from '../../stores/boardStore';
 	export let modalDetails;
-	$: console.log(modalDetails);
+
+	const { boardIndex, columnIndex, taskIndex } = modalDetails;
+
+	$: task = $boards[boardIndex].columns[columnIndex].tasks[taskIndex];
 </script>
