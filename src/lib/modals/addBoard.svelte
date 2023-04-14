0ffc15fd4b6f6adf49c42a4cc217aaeffbaf5c4b
davidthus <dvdnicorici05@gmail.com>
2023-04-14 23:30:01 +0100

    add: markup for addBoard component

--- src/lib/modals/addBoard.svelte
+++ src/lib/modals/addBoard.svelte
@@ -3,4 +3,20 @@
 </script>
 
 <h2 class="lightTextPrimary headingl">Add New Board</h2>
-<form />
+<form on:submit={handleSubmit}>
+	<InputGroup
+		name="title"
+		config={{ isTextArea: false, isError: $errors.title }}
+		placeholderText="e.g. Take coffee break"
+		errorMessage={$errors.title}
+		{handleChange}
+		value={$form.title}>Title</InputGroup
+	>
+	<InputGroup
+		config={{ isTextArea: true }}
+		{handleChange}
+		placeholderText="e.g. It’s always good to take a break. This 15 minute break will 
+	recharge the batteries a little."
+		value={$form.description}>Description</InputGroup
+	>
+</form>
