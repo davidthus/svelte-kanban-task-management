a453fdbb1f170c7c6540a80b938ed49e81f3c42c
davidthus <dvdnicorici05@gmail.com>
2023-04-27 20:54:53 +0100

    add: create form in editBoard script block

--- src/lib/modals/editBoard.svelte
+++ src/lib/modals/editBoard.svelte
@@ -1,5 +1,35 @@
 <script>
+	import { boards } from '../../stores/boardStore';
+	import InputGroup from '../inputGroup.svelte';
 	export let modalDetails;
+
+	$: ({ boardIndex } = modalDetails);
+	$: board = $boards[boardIndex];
+	$: boardColumns = $boards[boardIndex].columns;
+
+	const { form, handleChange, errors, handleSubmit } = createForm({
+		initialValues: {
+			name: board.name,
+			columns: board.columns
+		},
+		validate: (values) => {
+			let errs = {};
+			if (values.name === '') {
+				errs['name'] = "Can't be empty";
+			}
+			return errs;
+		},
+		onSubmit: (values) => {
+			alert(JSON.stringify(values));
+		}
+	});
+
+	const add = () => {
+		$form.columns = $form.columns.concat({ title: '', isCompleted: '' });
+	};
+	const remove = (i) => () => {
+		$form.columns = $form.columns.filter((u, j) => j !== i);
+	};
 </script>
 
 <h2 class="lightTextPrimary headingl">Add New Task</h2>
